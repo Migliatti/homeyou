88221a770aa41412ce50259ff79afd0fa80c4340
Gabriel Migliatti <101683285+Migliatti@users.noreply.github.com>
2023-08-29 17:06:55 -0300

    reinstalando dependencias

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Home from "./pages/Home";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Home from "pages/Home";
+import Header from "components/Header";
+import Footer from "components/Footer";
 
 function App() {
   return (
