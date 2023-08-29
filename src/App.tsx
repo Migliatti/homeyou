1b12deb2d02da8f1a91cf4fc9d036299d424d884
Gabriel Migliatti <101683285+Migliatti@users.noreply.github.com>
2023-08-29 16:31:00 -0300

    Atualizando os imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./components/header";
 import Home from "pages/home";
-import Footer from "components/footer";
+import Header from "components/Header";
+import Footer from "components/Footer";
 
 function App() {
   return (
